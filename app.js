236eeceaf3e39cec6402202ff0cc3e07b8f3b9eb
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-05-06 16:48:44 -0400

    added background video and changed form opacity

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const container = document.querySelector('.container');
 
 const headingSpan2 = document.querySelector('.heading-span-2');
 
-const form = document.querySelector('.form');
-
 const signUpBtn = document.querySelector('.signup-btn');
 const signInBtn = document.querySelector('.signin-btn');
 
+//form DOM constants
+const userName = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+const form = document.querySelector('.form');
+
+// button animations
 signUpBtn.addEventListener('click', () => {
   container.classList.add('change');
   setTimeout(() => {
@@ -23,4 +29,27 @@ signInBtn.addEventListener('click', () => {
   }, 200);
   form.className = 'form sign-in';
   //  clearForm();
+});
+
+const error = (input, message) => {
+  const inputWrapper = input.parentElement;
+  inputWrapper.className = 'input-wrapper error';
+  inputWrapper.querySelector('message').textContent = message;
+};
+
+// form validation
+
+const checkRequiredFields = (inputArr) => {
+  inputArr.forEach((input) => {
+    if (input.value.trim() === '') {
+      error(input, `${input.id} is required`);
+    } else {
+      //success
+    }
+  });
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  checkRequiredFields([userName, email, password, password2]);
 });
